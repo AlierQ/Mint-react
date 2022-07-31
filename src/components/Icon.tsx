7e30617f816ffb6acdeab77ae13244475e685474
AlierQ <1029369938@qq.com>
2022-07-31 21:20:19 +0800

    add props type:Props

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,5 +1,12 @@
-const Icon = (props: any): any => {
+type Props = {
+  className: string
+  color: string
+  size: string
+}
+
+const Icon = (props: Props) => {
   try {
+    // 动态引入svg，相当于按需加载
     require(`icons/${props.className}.svg`);
   } catch (error) {
     throw new Error('not find ' + props.className + ' svg file');
