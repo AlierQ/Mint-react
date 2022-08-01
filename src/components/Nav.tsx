dd41bf956a70317597dded3a3c8deea56147122b
AlierQ <1029369938@qq.com>
2022-08-01 08:42:51 +0800

    write Add top style

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,4 @@
-import {NavLink, Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import React from 'react';
 import styled from 'styled-components';
 import Icon from 'components/Icon';
--- src/pages/Add.tsx
+++ src/pages/Add.tsx
@@ -1,14 +1,54 @@
 import Nav from 'components/Nav';
 import Layout from 'components/Layout';
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+
+const Top = styled.div`
+  height: 80px;
+  background: #79c79f;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  padding-bottom: 10px;
+
+  .in,
+  .out {
+    font-size: 22px;
+    margin: 0 10px;
+    position: relative;
+
+    &.selected {
+      &::after {
+        content: "";
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        height: 2px;
+        background: #333;
+      }
+    }
+  }
+
+  .close {
+    position: absolute;
+    right: 20px;
+    font-size: 18px;
+  }
+`;
+const Content = styled.div``;
+const Bottom = styled.div``;
 
 const Add = () => {
   return (
     <Layout>
-      <div>这里是记账页面的头部</div>
-      <div>这里是记账页面的内容</div>
-      <div>
-        <Nav/>
-      </div>
+      <Top>
+        <div className="out selected">支出</div>
+        <div className="in selected">收入</div>
+        <Link to="/" className="close">取消</Link>
+      </Top>
+      <Content>这里是记账页面的内容</Content>
+      <Bottom><Nav/></Bottom>
     </Layout>
   );
 };
