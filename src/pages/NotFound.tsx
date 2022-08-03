9d1b2f0bd9f57d3e1b7ea88b43c165cb5bf93843
AlierQ <1029369938@qq.com>
2022-08-04 00:34:59 +0800

    complate NotFound

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,38 @@
 import Layout from 'components/Layout';
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import Icon from 'components/Icon';
+
+const Top = styled.div``;
+const Content = styled.div`
+  .page {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    > .toDetail {
+      margin-top: 10px;
+      color: #79c79f;
+      text-decoration: underline #79c79f;
+    }
+  }
+`;
+const Bottom = styled.div``;
 
 const NotFound = () => {
   return (
     <Layout>
-      <div/>
-      <div>这里是404页面</div>
-      <div/>
+      <Top/>
+      <Content>
+        <div className="page">
+          <Icon name="404" color="#79c79f" size="240"/>
+          <Link to="/" className="toDetail"><em>点击此处返回首页</em></Link>
+        </div>
+      </Content>
+      <Bottom/>
     </Layout>
   );
 };
