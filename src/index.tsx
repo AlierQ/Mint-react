d7b0385b228250a29269f312900c8bebc6e0c44f
AlierQ <1029369938@qq.com>
2022-08-04 17:32:12 +0800

    pc preview the prompt

--- src/index.tsx
+++ src/index.tsx
@@ -15,6 +15,10 @@ root.render(
   </React.StrictMode>
 );
 
+if (document.documentElement.clientWidth > 500) {
+  window.alert("请使用手机浏览器访问或者使用PC浏览器开发工具进行移动端预览");
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
